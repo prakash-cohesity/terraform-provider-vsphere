b97238f0d620eba2299b7249c955f0904e5f348d
Mradul Maheshwari <mradul.maheshwari@cohesity.com>
2020-02-28 08:15:19 -0800

    Deleting the VMDK disks and folder with terraform destroy operation

--- vsphere/internal/helper/structure/structure_helper.go
+++ vsphere/internal/helper/structure/structure_helper.go
@@ -349,6 +349,18 @@ func KiBToByte(n interface{}) float64 {
 
 // KiBToByte returns n*1024.
 //
+// The output is returned as float64 - if another type is needed, it needs to be
+// cast.
+func KiBToByte(n interface{}) float64 {
+	switch v := n.(type) {
+	case float64:
+		return float64(v * float64(1024))
+	}
+	panic(fmt.Errorf("non-integer type %T for value", n))
+}
+
+// KiBToByte returns n*1024.
+//
 // The output is returned as int64 - if another type is needed, it needs to be
 // cast. Remember that int32 overflows at around 2GiB and uint32 will overflow at 4GiB.
 func KiBToByte(n interface{}) float64 {
